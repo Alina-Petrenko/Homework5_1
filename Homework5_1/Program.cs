46722436cbcdd97c8baacc18097257d960b109f3
yomad <yomad@YOMADJINPC>
2022-01-31 16:19:27 +0200

    Add project files.

---
+++ Homework5_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
