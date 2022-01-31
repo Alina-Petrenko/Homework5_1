d53d2aebefd6e1b1bb9ab5efe73b3d5c0fb0218e
Alina Petrenko <petrenkoaalina@gmail.com>
2022-01-31 18:22:47 +0200

    Fix Get methods

--- Homework5_1/Program.cs
+++ Homework5_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,40 +14,37 @@ namespace Homework5_1
         {
             using (var http = new HttpClient())
             {
-                Console.WriteLine("'GET' List Users");
-                var listUsers = await http.GetAsync("https://reqres.in/api/users?page=2");
-                Console.WriteLine($"Status: {listUsers.StatusCode}\n");
 
-                Console.WriteLine($"'GET' Single User");
-                var singleUser = await http.GetAsync("https://reqres.in/api/users/2");
-                Console.WriteLine($"Status: {singleUser.StatusCode}\n");
 
-                Console.WriteLine("'GET' Single User Not Found");
-                var singleUserNotFound = await http.GetAsync("https://reqres.in/api/users/23");
-                Console.WriteLine($"Status: {singleUserNotFound.StatusCode}\n");
+                
 
-                Console.WriteLine("'GET' List <Resource>");
-                var listResource = await http.GetAsync("https://reqres.in/api/unknown");
-                Console.WriteLine($"Status: {listResource.StatusCode}\n");
 
-                Console.WriteLine("'GET' Single <Resource>");
-                var singleResource = await http.GetAsync("https://reqres.in/api/unknown/2");
-                Console.WriteLine($"Status: {singleResource.StatusCode}\n");
+                //Console.WriteLine("'GET' Single User Not Found");
+                //var singleUserNotFound = await http.GetAsync("https://reqres.in/api/users/23");
+                //Console.WriteLine($"Status: {singleUserNotFound.StatusCode}\n");
 
-                Console.WriteLine("'GET' Single <Resource> Not Found");
-                var singleNotFound = await http.GetAsync("https://reqres.in/api/unknown/23");
-                Console.WriteLine($"Status: {singleNotFound.StatusCode}\n");
+                //Console.WriteLine("'GET' List <Resource>");
+                //var listResource = await http.GetAsync("https://reqres.in/api/unknown");
+                //Console.WriteLine($"Status: {listResource.StatusCode}\n");
 
-                Console.WriteLine("'POST' Create");
-                User user = new User
-                {
-                    name = "morpheus",
-                    job = "leader"
+                //Console.WriteLine("'GET' Single <Resource>");
+                //var singleResource = await http.GetAsync("https://reqres.in/api/unknown/2");
+                //Console.WriteLine($"Status: {singleResource.StatusCode}\n");
 
-                };
-                string serializedUser = JsonConvert.SerializeObject(user);
-                var create = await http.PostAsync("https://reqres.in/api/users", new StringContent(serializedUser, Encoding.UTF8, "application/json"));
-                Console.WriteLine($"Status: {create.StatusCode}\n");
+                //Console.WriteLine("'GET' Single <Resource> Not Found");
+                //var singleNotFound = await http.GetAsync("https://reqres.in/api/unknown/23");
+                //Console.WriteLine($"Status: {singleNotFound.StatusCode}\n");
+
+                //Console.WriteLine("'POST' Create");
+                //User user = new User
+                //{
+                //    name = "morpheus",
+                //    job = "leader"
+
+                //};
+                //string serializedUser = JsonConvert.SerializeObject(user);
+                //var create = await http.PostAsync("https://reqres.in/api/users", new StringContent(serializedUser, Encoding.UTF8, "application/json"));
+                //Console.WriteLine($"Status: {create.StatusCode}\n");
 
                 //Console.WriteLine("'PUT' Update");
                 //var update = await http.PutAsync("https://reqres.in/api/unknown/23");
---
+++ Homework5_1/Starter.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_1
+{
+    class Starter
+    {
+        public async Task GetMethodsAsync()
+        {
+            using (var http = new HttpClient())
+            {
+                Console.WriteLine("'GET' List Users");
+                var listUsers = await http.GetAsync("https://reqres.in/api/users?page=2");
+                Console.WriteLine($"Status: {listUsers.StatusCode}");
+                string newlistUsers = await listUsers.Content.ReadAsStringAsync();
+                var newlistUsersResult = JsonConvert.DeserializeObject<Page>(newlistUsers);
+                Console.WriteLine($"Page: { newlistUsersResult.page}, Total: {newlistUsersResult.total}, Per Page: {newlistUsersResult.per_page}, Total Pages: {newlistUsersResult.total_pages}\nSupport Text: {newlistUsersResult.support.text}\nSupport Url: {newlistUsersResult.support.url}");
+                foreach (var item in newlistUsersResult.data)
+                {
+                    Console.WriteLine($"Id: {item.id}, Full Name: { item.first_name} { item.last_name}, Email: {item.email}, Avatar: {item.avatar} ");
+                }
+
+                Console.WriteLine($"'GET' Single User");
+                var singleUser = await http.GetAsync("https://reqres.in/api/users/2");
+                Console.WriteLine($"Status: {singleUser.StatusCode}\n");
+                string newSingleUser = await singleUser.Content.ReadAsStringAsync();
+                var singleUserResult = JsonConvert.DeserializeObject<SingleUser>(newSingleUser);
+                Console.WriteLine($"Id: {singleUserResult.data.id}, Full Name: {singleUserResult.data.first_name} {singleUserResult.data.last_name}, " +
+                    $"Email: {singleUserResult.data.email}, Avatar: {singleUserResult.data.avatar}" +
+                    $"\nSupport Text: {singleUserResult.support.text}" +
+                    $"\nSupport Url: {singleUserResult.support.url}");
+
+
+            }
+        }
+    }
+}
